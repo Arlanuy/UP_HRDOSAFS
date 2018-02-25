de51d8921e5dcf4c637cc15b5ad8ca1d25503810
Arlan <arlanvincentuy@gmail.com>
2018-02-26 04:36:33 +0800

    fix production.rb to model heroku setting changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-   config.action_mailer.default_url_options = { host: 'https://up-hrdo-safs-rails.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://up-hrdosafs.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
